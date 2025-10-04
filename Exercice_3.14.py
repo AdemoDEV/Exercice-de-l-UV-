b47d1a05ffec489494447552037b446e45f7f78c
AdeMo <58342559+AdemoDEV@users.noreply.github.com>
2025-10-04 15:21:23 +0200

    Update Exercice_3.14.py

--- Exercice_3.14.py
+++ Exercice_3.14.py
@@ -1,16 +1,16 @@
 import random
-NB_ESSAIS_MAX = 6
 secret = random.randint(0, 100)
 
 essais = 0
-while essais < NB_ESSAIS_MAX:
+while essais < 6:
     essaie_donnee = int(input())
     essais += 1
 
     if essaie_donnee == secret:
         print(f"Gagné en {essais} essai(s) !")
         break
-    if essais < NB_ESSAIS_MAX:
+    if essais < 6:
         print("Trop grand" if essaie_donnee > secret else "Trop petit")
 else:
     print(f"Perdu ! Le secret était {secret}")
+
